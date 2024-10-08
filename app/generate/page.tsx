0283db023d0ac61377b9ed02d743512f9014c5d6
zohaib240 <zohaibjs123@gmail.com>
2024-10-08 09:33:14 -0700

    update page.tsx

--- app/generate/page.tsx
+++ app/generate/page.tsx
@@ -2,27 +2,23 @@
 import React, { useState, useEffect, useRef } from 'react'
 import Image from 'next/image';
 
+
 const Generate = ({ searchParams }: { searchParams: { id: string, url: string, box: number } }) => {
   console.log(searchParams);
 
   const [hello, setHello] = useState(searchParams.box)
   const [meme,setImage] = useState<string | null>(null)
  
-  let input1 = useRef<(HTMLInputElement)>(null)
-  let input2 = useRef<(HTMLInputElement)>(null)
-  let input3 = useRef<(HTMLInputElement)>(null)
-  let input4 = useRef<(HTMLInputElement)>(null)
-   
- const postMeme = async (event: React.FormEvent<HTMLFormElement>) =>{
-  event.preventDefault();
-  const data = await fetch(`https://api.imgflip.com/caption_image?template_id=${searchParams.id}&username=zohaibjs123&password=123456Aa@&text0=${input1.current?.value}&text1=${input2.current?.value}&text2=${input3.current?.value}&text3=${input4.current?.value}`, {
-    method: 'POST',
-}); 
-   const respone = await data.json()
+  let input1 = useRef<HTMLInputElement>(null);
+  let input2 = useRef<HTMLInputElement>(null);
+
+  const postMeme = async (event: any ) =>{
+   event.preventDefault();
+   const data = await fetch(`https://api.imgflip.com/caption_image?template_id=${searchParams.id}&username=zohaibjs123&password=123456Aa@&text0=${input1.current?.value}&text1=${input2.current?.value}`); 
+  const respone = await data.json()
    console.log(respone);
    setImage(respone.data.url)
  }
-
 
   useEffect(() => {
     setHello(searchParams.box);
@@ -30,69 +26,31 @@ const Generate = ({ searchParams }: { searchParams: { id: string, url: string, b
 
   return (
     <>
-      <div className='flex justify-center'>
-        <Image src={searchParams.url} width={300} height={300} alt='no-image ' />
+      <div className='flex justify-center '>
+        <Image src={searchParams.url} width={300} height={300} alt='no-image ' className='border border-black rounded' />
       </div>
       {
-        hello == 2 ? (
-          <div className='flex justify-center gap-5 mt-5 flex-wrap'>
-       <form onSubmit={postMeme}>
+        hello ==2 ? (
+          <div className='flex justify-center gap-5 mt-5 flex-wrap '>
+       <form onSubmit={postMeme} >
        <input
               type="text"
               placeholder="Type here"
-              className="input input-bordered input-accent w-full max-w-xs" ref={input1} /> <br /> <br />
+              className="input input-bordered bg-blue-100 text-black w-full max-w-xs rounded-md p-2 border-2 border-black"ref={input1} /> <br /> <br />
             <input
               type="text"
               placeholder="Type here"
-              className="input input-bordered input-accent w-full max-w-xs" ref={input2}  /> <br /> <br />
-            <button className='bg-blue-500 text-white mt-3 text-sm px-4 py-2 rounded' type='submit'>Generate Meme</button>
-       </form>
-          </div>
-        ) : hello == 3 ? (
-          <div className='flex justify-center gap-5 mt-5'> 
-            <form onSubmit={postMeme}>
-       <input
-              type="text"
-              placeholder="Type here"
-              className="input input-bordered input-accent w-full max-w-xs" ref={input1} /> <br /> <br />
-            <input
-              type="text"
-              placeholder="Type here"
-              className="input input-bordered input-accent w-full max-w-xs" ref={input2}  /> <br /> <br />
-            <input
-              type="text"
-              placeholder="Type here"
-              className="input input-bordered input-accent w-full max-w-xs" ref={input3}  /> <br /> <br />
-            <button className='bg-blue-500 text-white mt-3 text-sm px-4 py-2 rounded' type='submit'>Generate Meme</button>
-       </form>
-          </div>
-        ) : hello == 4 ? (
-          <div className='flex justify-center gap-5 mt-5'>
-           <form onSubmit={postMeme}>
-       <input
-              type="text"
-              placeholder="Type here"
-              className="input input-bordered input-accent w-full max-w-xs" ref={input1} /> <br /> <br />
-            <input
-              type="text"
-              placeholder="Type here"
-              className="input input-bordered input-accent w-full max-w-xs" ref={input2}  /> <br /> <br />
-            <input
-              type="text"
-              placeholder="Type here"
-              className="input input-bordered input-accent w-full max-w-xs" ref={input3}  /> <br /> <br />
-            <input
-              type="text"
-              placeholder="Type here"
-              className="input input-bordered input-accent w-full max-w-xs" ref={input4}  /> <br /> <br />
-            <button className='bg-blue-500 text-white mt-3 text-sm px-4 py-2 rounded' type='submit'>Generate Meme</button>
+              className="input input-bordered bg-blue-100 text-black w-full max-w-xs rounded-md p-2 border-2 border-black"ref={input2}  /> <br /> <br />
+            <div className= ' flex justify-center'><button className= '  bg-blue-500 text-white mt-3 text-sm px-4 py-2 rounded' type='submit'>Generate Meme</button></div>
        </form>
           </div>
         ) : (
           <div>Loading...</div>
         )
       }
-      {meme ? <Image src={meme} width={200} height={200} alt="loading" /> :null}
+      <div className='flex justify-center mt-3 '>
+{meme ? <Image src={meme} width={300} height={300} alt="loading" className='border rounded border-black' /> :null}
+      </div>
     </>
   )
 } 
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import Image from 'next/image';
 import Link from 'next/link';
 
-
-
 const page = async () => {
  const memes = await fetch('https://api.imgflip.com/get_memes')
  const response = await memes.json()
@@ -13,20 +11,24 @@ const page = async () => {
     <>
     <div className='flex justify-center gap-2 mt-4'>
       <div className='animate-bounce bg-white'>
-      <Image className="object-contain bg-white" key={1} src={'http://w7.pngwing.com/pngs/761/310/png-transparent-internet-troll-internet-meme-trollface-laughing-face-meme-sticker-miscellaneous-face-food-thumbnail.png'} width={60} height={60}  alt='no-img'/>
+      <Image className="object-contain  bg-white" key={1} src={'http://w7.pngwing.com/pngs/761/310/png-transparent-internet-troll-internet-meme-trollface-laughing-face-meme-sticker-miscellaneous-face-food-thumbnail.png'} width={60} height={60}  alt='no-img'/>
      </div>
       <div>
-        <h1 className='text-center mt-5 text-3xl font-bold '>Meme Generater App </h1> 
+        <h1 className='text-center mt-5 text-3xl font-bold '>Meme Generator App </h1> 
       </div>
       </div>
     <div className='bg-red-400 text-white align-center justify-center mt-3 flex animate-pulse'>
 If you Want To Generate Memes !
       </div>
-      <div className='flex flex-wrap justify-center mt-10 gap-3 m-3'>
-        {response.data.memes.map((item: any) => (
-          <div key={item.id} className='flex flex-col items-center border-2 border-black rounded-lg '>
-            <Image src={item.url} width={200} height={200} alt='no-img' />
-            <button className='bg-blue-500 text-white mt-3 text-sm px-4 py-2 rounded'>
+      <div className='flex flex-wrap justify-center mt-10 gap-5 m-3'>
+        {response.data.memes.filter((item : any ) => item.box_count === 2).map((item: any) => (
+          <div key={item.id} className='flex flex-col items-centertilt-in-top-1 hover:relative hover:bottom-2 hover:shadow-lg hover:border hover: transition-colors: 150ms border border-black rounded-lg p-3 '>
+            <div style={{
+              borderTopLeftRadius: '18px',
+              borderTopRightRadius: '18px'
+            }} className='flex justify-center bg-[#e9e9e9] items-center'>
+              <Image className='h-[200px]' src={item.url} width={200} height={200} alt='item.name' /> </div>
+            <button className='bg-blue-500 text-white mt-3 text-sm px-4 py-2 rounded '>
   <Link
     href={{
       pathname: '/generate',
@@ -34,6 +36,8 @@ If you Want To Generate Memes !
         url: item.url,
         id: item.id,
         box: item.box_count,
+        name: item.name,
+
       },
     }}
   >Generate Meme
